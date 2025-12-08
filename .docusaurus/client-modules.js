0cf9c190f5210e55b698b5433bd48a0e8d085407
syedabanog-1 <gulzar_bano@yahoo.com>
2025-12-08 10:32:55 +0500

    add commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,9 @@
+export default [
+  require("D:\\syeda Gulzar Bano\\Hackathon-1-AIBOOK\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
+  require("D:\\syeda Gulzar Bano\\Hackathon-1-AIBOOK\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("D:\\syeda Gulzar Bano\\Hackathon-1-AIBOOK\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("D:\\syeda Gulzar Bano\\Hackathon-1-AIBOOK\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("D:\\syeda Gulzar Bano\\Hackathon-1-AIBOOK\\src\\css\\custom.css"),
+  require("D:\\syeda Gulzar Bano\\Hackathon-1-AIBOOK\\src\\css\\sidebar-custom.css"),
+  require("D:\\syeda Gulzar Bano\\Hackathon-1-AIBOOK\\src\\components\\Root.tsx"),
+];
